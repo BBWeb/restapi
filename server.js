f7efcd6c244b14f79c8a3816c2045f3fec3a73a7
cjblomqvist <carl@cjblomqvist.com>
2016-03-07 19:09:17 +0100

    Fix CORS properly

--- server.js
+++ server.js
@@ -157,10 +157,9 @@ function ensureKey(req, res, next) {
 }
 
 function enableCORS(req, res, next) {
-  res.header['Access-Control-Allow-Origin'] = '*';
-  res.header['Access-Control-Allow-Credentials'] = true;
-  res.header['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS';
-  res.header['Access-Control-Allow-Headers'] = 'Content-Type';
+  res.header('Access-Control-Allow-Origin', '*'); 
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
   next();
 }
